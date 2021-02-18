e0678e6715308868779e3841cc6d9ad05500ce74
Fernando Lopez <37802572+Cainuriel@users.noreply.github.com>
2021-02-18 17:24:56 +0100

    Upload files

---
+++ jotason.js
@@ -0,0 +1,15 @@
+const tree = {
+  tag: "ul",
+  children: [
+    {
+      text: "Primer elemento"
+    },
+    {
+      text: "Cáspita, otro elemento"
+    },
+    {
+    	text: "El último"
+    }
+  ]
+};
+
